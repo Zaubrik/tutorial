972c8bd36955f8fc531f0f041f7ec796d58996f9
timonson <timo.rossa@tutanota.com>
2024-05-10 15:32:19 +0200

    Update

--- website/static/core/core.js
+++ website/static/core/core.js
@@ -4,17 +4,19 @@ import {
   setFooterColorByPreviousSection,
 } from "/deps.js";
 
-window.addEventListener("load", ensureIdScrolling());
-
 const coreResources = [{
   url: "/core/html/navbar.html",
   selector: "universal-navbar",
   parentSelector: "navbar",
 }, {
   url: "/core/html/footer.html",
-  selector: "universal-footer",
-  parentSelector: "body > footer",
+  selector: "#footer",
+  parentSelector: "body",
 }];
 
 appendHtmlResource(coreResources);
-setFooterColorByPreviousSection(["#f6f9fc", "#ffffff"]);
+
+window.addEventListener("load", (event) => {
+  ensureIdScrolling();
+  setFooterColorByPreviousSection(["#f6f9fc", "#ffffff"]);
+});
